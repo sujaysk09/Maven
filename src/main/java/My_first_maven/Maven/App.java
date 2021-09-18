7ba053a8cf294a5be0113b49971134fe8e35bcdb
Admin <Admin@Lenovo-PC>
2021-09-18 23:12:45 +0530

    Code1

---
+++ src/main/java/My_first_maven/Maven/App.java
@@ -0,0 +1,13 @@
+package My_first_maven.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/new_start/start1.java
@@ -0,0 +1,11 @@
+package new_start;
+
+public class start1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("my first project");
+
+	}
+
+}
